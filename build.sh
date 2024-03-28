03a41bbffae559cd845d949eec02f3b86d354139
邹海强 <15591017398@163.com>
2024-03-28 21:10:29 +0800

    update

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 
 hugo -D
 
+git config --global core.autocrlf true
 git remote add origin git@github.com:bitfrost7/myblog.git
 git add .
 git commit -m "update"
 git push origin master
 
 cd public
-
+git config --global core.autocrlf true
 git remote add origin git@github.com:bitfrost7/bitfrost7.github.io.git
 git add .
 git commit -m "update"
